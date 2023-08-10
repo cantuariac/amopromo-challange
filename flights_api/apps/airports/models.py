f9454daa160878397af41e87256470b6ee874470
Caio Cantuária <caio.cantuaria@gmail.com>
2023-08-09 22:39:44 -0300

    created Airport model

---
+++ flights_api/apps/airports/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+class Airport(models.Model):
+    iata = models.CharField(_("IATA"), max_length=3)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.iata
+    
